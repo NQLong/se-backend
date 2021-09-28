bc42abd7a4dd968267f057b2d33f5bf48240f34d
Ben <nqlong0709@gmail.com>
2021-09-28 13:55:20 +0700

    Format code, fix related name, table name, docker db image, add migrations

--- backend/restaurant/models/restaurant_banner.py
+++ backend/restaurant/models/restaurant_banner.py
@@ -4,11 +4,11 @@ from user_account.models import User
 from media_file.models import Media
 from restaurant.models import Restaurant
 class  RestaurantBanner(AbstractModel):
-        class Meta:
-            db_table = 'restaurant'
-        image=models.ForeignKey(to=Media, related_name='image_restaurant_banner', on_delete=models.CASCADE, null=False)
-        restaurant=models.ForeignKey(to=Restaurant, related_name='restaurant_restaurant_banner', on_delete=models.CASCADE, null=False)
-        description=models.TextField(null=False)
-        creator=models.ForeignKey(to=User, related_name='creator_restaurant_bannner', on_delete=models.CASCADE, null=False)
-        ordinal=models.IntegerField(null=False)
+    class Meta:
+        db_table = 'restaurant_banner'
+    image = models.ForeignKey(to=Media, related_name='image_restaurant_banner', on_delete=models.CASCADE, null=False)
+    restaurant = models.ForeignKey(to=Restaurant, related_name='restaurant_restaurant_banner', on_delete=models.CASCADE, null=False)
+    description = models.TextField(null=True, blank=True)
+    creator = models.ForeignKey(to=User, related_name='creator_restaurant_banner', on_delete=models.CASCADE, null=False)
+    ordinal = models.IntegerField(null=False)
 
--- backend/restaurant/models/table.py
+++ backend/restaurant/models/table.py
@@ -3,12 +3,13 @@ from django.db.models.fields import IntegerField
 from utils.models import AbstractModel
 from user_account.models import User
 from restaurant.models.restaurant import Restaurant
+
 class Table(AbstractModel):
-        class Meta:
-            db_table = 'restaurant'
-        ordinal=models.IntegerField(null=False)
-        restaurant=models.ForeignKey(to=Restaurant,related_name='restaurant_table',on_delete=models.CASCADE,null=False)
-        description=models.TextField(null=False)
-        creator=models.ForeignKey(to=User,related_name='creator_table',on_delete=models.CASCADE,null=False)
-        status=models.IntegerField(null=False)
-        capacity=models.IntegerField(null=False)
+    class Meta:
+        db_table = 'table'
+    ordinal = models.IntegerField(null=False)
+    restaurant = models.ForeignKey(to=Restaurant, related_name='restaurant_table', on_delete=models.CASCADE, null=False)
+    description = models.TextField(null=True, blank=True)
+    creator = models.ForeignKey(to=User, related_name='creator_table', on_delete=models.CASCADE, null=False)
+    status = models.CharField(max_length=32, null=False)
+    capacity = models.IntegerField(null=False)
